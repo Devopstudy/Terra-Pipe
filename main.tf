4c6f78da8fee4fc8c96a8a4e1651d237ced76d27
mohitstudy <mohitstudy@gmail.com>
2025-07-10 11:34:38 +0530

    deleted

--- main.tf
+++ main.tf
@@ -7,12 +7,3 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "def13e29-f65a-496b-b10e-3cffca9a480f"
-}
-
-resource "azurerm_resource_group" "rg-update" {
-  name     = "rg-update"
-  location = "central india"
-}
