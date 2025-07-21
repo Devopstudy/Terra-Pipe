0c63cbc246b889a158df04333c3c4e5f29568e07
mohitstudy <mohitstudy@gmail.com>
2025-07-21 13:33:04 +0530

    new code

--- main.tf
+++ main.tf
@@ -1,24 +1,38 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.0.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "def13e29-f65a-496b-b10e-3cffca9a480f"
-}
 
 
-resource "azurerm_resource_group" "mickey" {
-  name     = "mickey"
+resource "azurerm_resource_group" "rg-sky" {
+  name     = "rg-sky"
   location = "central india"
 }
 
-resource "azurerm_resource_group" "ericsson" {
-  name     = "ericsson"
-  location = "central india"
+resource "azurerm_storage_account" "aliendisk" {
+    depends_on = [azurerm_resource_group.rg-sky]
+  name                     = "alienddisk"
+  resource_group_name      = "rg-sky"
+  location                 = "central india"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+    depends_on = [azurerm_storage_account.aliendisk]
+	name = "tfstate"
+	storage_account_name = "alienddisk"
+	container_access_type  ="private"
+	}
+
+resource "azurerm_virtual_network" "alien-network" {
+    depends_on          = [azurerm_resource_group.rg-sky]
+    name                = "alien-network"
+  resource_group_name = "rg-sky"
+    location            = "central india"
+    address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "alien-subnet" {
+    depends_on           = [azurerm_virtual_network.alien-network]
+    name                 = "alien-subnet"
+    resource_group_name  = "rg-sky"
+    virtual_network_name = "alien-network"
+    address_prefixes     = ["10.0.1.0/24"]
 }
